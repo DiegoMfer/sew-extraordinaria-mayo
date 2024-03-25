a2a480196130c36fa718c0f184ba8910d6bcce3b
Diego Martín Fernández <diegomartinfnz@gmail.com>
2024-03-25 12:35:55 +0100

    apis funcionando, queda google maps

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ class Index {
         this.carpeta = 'multimedia/image/'
         this.textoImagen = 'Imagen de las Islas Baleares'
         this.index = 0
-        this.key = "c4ed265dd950cde9fc4d2b46bf0ee682";
     }
 
     anterior(){
@@ -24,9 +23,71 @@ class Index {
         $('main section:eq(0) img').attr("src",this.carpeta  + this.carrusel[this.index]);
     }
 
+    //sexip11104@rockdian.com 9!RSg66*Ro*^
+    //jixel20241@shaflyn.com 4b599a13e3434e658a83954f78d06ae4 https://newsapi.org/docs/get-started#search
+    //jixel20241@shaflyn.com https://newsdata.io/documentation/#first-api-request
+    cargarNoticias(){
+      
+        var settings = {
+            "url": "https://newsdata.io/api/1/news?apikey=pub_40700bc2ea6eab212dcda37877ccfb028a0d8&q=islas baleares",
+            "method": "GET",
+            "timeout": 0,
+          };
 
-   
+          
+
+          $.ajax(settings).done(function(response) {
+            var datos = "";
+            $.each(response.results, function(index, article) {
+                datos += "<p> Título: " + article.title + "</p>";
+                datos += "<p> Descripción: " + article.description + "</p>";
+                datos += "<p> Enlace: <a href='" + article.link + "'>" + article.link + "</a></p>";
+                datos += "<p> ------------------------------------------------ </p>";
+            });
+            $("main section:eq(2)").append(datos);
+            $("main section:eq(2) button").attr("disabled", "disabled");
+        });
+    }
+
+
+    cargarDatosMeteorológicos() {
+
+        var settings = {
+            dataType:"json",
+            url:
+              "https://api.openweathermap.org/data/2.5/weather?q=Palma de Mallorca,ES&units=metric&lang=es&APPID=ef31978d6c1730f185266d2fcfef78b8",
+            method: "GET",
+            timeout: 0,
+          };
+      
     
+    
+        $.ajax(settings).done(function (datos) {
+            var meteo = ("<p>Datos de las Islas Baleares</p>")
+             meteo +=("<p>País: " +  datos.sys.country + "</p>");
+             meteo +=("<p>Latitud: " +  datos.coord.lat + " grados</p>");
+             meteo +=("<p>Longitud: " +  datos.coord.lon + " grados</p>");
+             meteo +=("<p>Temperatura: " +  datos.main.temp + " grados Celsius</p>");
+             meteo +=("<p>Temperatura máxima: " +  datos.main.temp_max + " grados Celsius</p>");
+             meteo +=("<p>Temperatura mínima: " +  datos.main.temp_min + " grados Celsius</p>");
+             meteo +=("<p>Presión: " +  datos.main.pressure + " milímetros</p>");
+             meteo +=("<p>Humedad: " +  datos.main.humidity + "%</p>"); 
+             meteo +=("<p>Amanece a las: " + new Date( datos.sys.sunrise *1000).toLocaleTimeString() + "</p>"); 
+             meteo +=("<p>Oscurece a las: " + new Date( datos.sys.sunset *1000).toLocaleTimeString() + "</p>"); 
+             meteo +=("<p>Dirección del viento: " +  datos.wind.deg + "  grados</p>");
+             meteo +=("<p>Velocidad del viento: " +  datos.wind.speed + " metros/segundo</p>");
+             meteo +=("<p>Hora de la medida: " + new Date( datos.dt *1000).toLocaleTimeString() + "</p>");
+             meteo +=("<p>Fecha de la medida: " + new Date( datos.dt *1000).toLocaleDateString() + "</p>");
+             meteo +=("<p>Descripción: " +  datos.weather[0].description + "</p>");
+             meteo +=("<p>Visibilidad: " +  datos.visibility + " metros</p>");
+             meteo +=("<p>Nubosidad: " +  datos.clouds.all + " %</p>");
+    
+             $("main section:eq(1)").append(meteo)
+             $("main section:eq(1) button").attr("disabled","disabled");
+            
+        });   
+    }
+
     ultimaVezActualizado(){
       $("main section:eq(5)").append("<p>"+ document.lastModified+"</p>")
     }
