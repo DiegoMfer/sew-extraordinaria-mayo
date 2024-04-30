476029fa631b3a31cc82b07425c045450273e03d
Diego <diegomartinfnz@gmail.com>
2024-04-30 13:15:11 +0200

    test

--- test.php
+++ test.php
@@ -4,22 +4,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-// MySQL server credentials
-$servername = "localhost"; // Change this if your MySQL server is on a different host
-$username = "your_username"; // Change this to your MySQL username
-$password = "your_password"; // Change this to your MySQL password
-$database = "your_database"; // Change this to your MySQL database name
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
 } else {
-    echo "Connected successfully";
+    echo 'Phew we have it!';
 }
 
-// Close connection
-mysqli_close($conn);
 ?>
