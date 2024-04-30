b6f61017f9effa6bbcb9f5fe27f90759aa7db68d
Diego Martín Fernández <diegomartinfnz@gmail.com>
2024-04-30 09:57:22 +0200

    mysql fix

--- php/bd.sql
+++ php/bd.sql
@@ -17,7 +17,7 @@ CREATE TABLE Recurso_turistico (
     nombre VARCHAR(100) NOT NULL,
     tipo VARCHAR(50) NOT NULL,
     descripcion TEXT,
-    limite_ocupacion INT,
+    limite_ocupacion INT NOT NULL,
     precio DECIMAL(10,2) NOT NULL
 );
 
@@ -54,12 +54,12 @@ INSERT INTO Recurso_turistico (nombre, tipo, descripcion, limite_ocupacion, prec
 VALUES 
     ('Cala Llombards', 'Playa', 'Una pequeña y pintoresca cala en Mallorca, perfecta para relajarse y nadar.', 200, 10.00),
     ('Turismo Rural en Ibiza', 'Rural', 'Experimenta la autenticidad de Ibiza alojándote en hermosas casas rurales.', 8, 120.00),
-    ('Cicloturismo por Formentera', 'Bicicleta', 'Descubre la belleza de Formentera en bicicleta, recorriendo sus caminos rurales y playas vírgenes.', NULL, 0.00),
+    ('Cicloturismo por Formentera', 'Bicicleta', 'Descubre la belleza de Formentera en bicicleta, recorriendo sus caminos rurales y playas vírgenes.', 100, 0.00),
     ('Cueva del Drach', 'Natural', 'Explora las impresionantes cuevas subterráneas en Mallorca, adornadas con estalactitas y estalagmitas.', 100, 25.00),
-    ('Iglesia de Sant Miquel', 'Monumento', 'Visita esta encantadora iglesia en Ibiza, con una arquitectura tradicional y vistas panorámicas.', NULL, 3.50),
-    ('Parque Natural de Mondragó', 'Parque', 'Sumérgete en la naturaleza en este parque protegido en Mallorca, hogar de una variedad de flora y fauna.', NULL, 0.00),
-    ('Mercado de Pescado de Ciutadella', 'Mercado', 'Disfruta de la autenticidad de Menorca explorando este bullicioso mercado de pescado.', NULL, 0.00),
-    ('Faro de Cap de Barbaria', 'Faro', 'Contempla las impresionantes vistas desde el Faro de Cap de Barbaria en Formentera, especialmente al atardecer.', NULL, 4.00),
-    ('Fiesta de Sant Joan', 'Evento', 'Únete a la celebración de la Fiesta de Sant Joan en Mallorca, llena de tradición, música y fuegos artificiales.', NULL, 0.00),
-    ('Necrópolis de Puig des Molins', 'Arqueológico', 'Explora las antiguas tumbas fenicias en este fascinante sitio arqueológico en Ibiza.', NULL, 7.00);
+    ('Iglesia de Sant Miquel', 'Monumento', 'Visita esta encantadora iglesia en Ibiza, con una arquitectura tradicional y vistas panorámicas.', 100, 3.50),
+    ('Parque Natural de Mondragó', 'Parque', 'Sumérgete en la naturaleza en este parque protegido en Mallorca, hogar de una variedad de flora y fauna.', 100, 0.00),
+    ('Mercado de Pescado de Ciutadella', 'Mercado', 'Disfruta de la autenticidad de Menorca explorando este bullicioso mercado de pescado.', 100, 0.00),
+    ('Faro de Cap de Barbaria', 'Faro', 'Contempla las impresionantes vistas desde el Faro de Cap de Barbaria en Formentera, especialmente al atardecer.', 100, 4.00),
+    ('Fiesta de Sant Joan', 'Evento', 'Únete a la celebración de la Fiesta de Sant Joan en Mallorca, llena de tradición, música y fuegos artificiales.', 100, 0.00),
+    ('Necrópolis de Puig des Molins', 'Arqueológico', 'Explora las antiguas tumbas fenicias en este fascinante sitio arqueológico en Ibiza.', 100, 7.00);
 
