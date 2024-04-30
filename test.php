b9a6d1c72cc69c16a768b46a6d8c1c41d38cb006
Diego <diegomartinfnz@gmail.com>
2024-04-30 13:12:22 +0200

    test

--- test.php
+++ test.php
@@ -1,25 +1,25 @@
+
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-session_start();
-$servername = "localhost";
-$username = "test";
-$password = "test";
-$dbname = "sew";
-
+// MySQL server credentials
+$servername = "localhost"; // Change this if your MySQL server is on a different host
+$username = "your_username"; // Change this to your MySQL username
+$password = "your_password"; // Change this to your MySQL password
+$database = "your_database"; // Change this to your MySQL database name
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 } else {
     echo "Connected successfully";
 }
 
 // Close connection
-$conn->close();
-
+mysqli_close($conn);
 ?>
